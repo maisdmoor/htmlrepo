e0bad86d2c4e836d3c830def48447b04e5758489
mais dmoor <maisdmoor1995@gmail.com>
2021-01-30 19:55:03 +0200

    m

---
+++ labo5.js
@@ -0,0 +1,26 @@
+var product = prompt ('masterpieces or accessories');
+
+while (product !== 'masterpieces' && product !== 'accessories'){
+
+    product = prompt ('masterpieces or accessories');
+    
+    }
+    
+    var youprefer = '';
+    
+    if ( product === 'masterpieces'){
+        youprefer = '<img src="./image/master.jpg "/>';
+    }else if ( product === 'accessories'){
+        youprefer = '<img src="./image/access.jpg"/>';
+    }
+    
+    var noof = prompt ('how many pieces do you want?');
+    var result ='';
+    
+    for (var i = 0 ; i < noof ; i++){
+    result = result + yourprefer;
+    
+    }
+    
+    document.write(result);
+    alert(' :) ')
