26a6f8386ce0f07d76e31f7b79df1f7fbcb08b71
mais dmoor <maisdmoor1995@gmail.com>
2021-01-31 23:15:52 +0200

    m

--- app.js
+++ app.js
@@ -1,53 +1,37 @@
-//function greeting(){
-  var hourNow = prompt('what is the time');
-  console.log(hourNow)
-  // 
-  var message= '';
-  console.log(message);
-  if(hourNow >= 18  && hourNow <= 24){ //f
-      message = 'Good Evening';
-  }else if(hourNow >= 12 && hourNow < 18){
-      message = 'Good Afternoon';
-  }else if (hourNow >= 0 && hourNow< 12){
-      message = 'Good morning';
-  }else{
-      alert('Welcome ')
-  }
-  document.write('<h2>' + message + '</h2>');
+function product(){
+    var product = prompt ('masterpieces or accessories');
 
- // }
-//greeting();
 
-  //function orderingCookie(){
-  var userOrder = prompt('what would you like to order Masterpieces or Accessories','place your order');
-  while(!(userOrder =='Masterpieces' || userOrder =='Accessories')){
-            
-                  
-      userOrder = prompt('please enter the correct item')
-  }
-  
-  
-  var userInputs = prompt('how many Pieces do you want?')
-  // 5 
-  var image=''; 
-  
-  // docuemnt.write()
-  // 3
-  for(var i = 0; i < userInputs; i++){
-      if(userOrder =='Masterpieces'){
-          image = image + '<img src="">'
-          console.log(image)
-          
-      }else if(userOrder =='Accessories'){
-          image = image +'<img src="">'
+while (product !== 'masterpieces' && product !== 'accessories'){
+
+    product = prompt ('masterpieces or accessories');
     
-      }
+    }
+}
+//function Product1(){
+    var youprefer = '';
+    
+    if ( product === 'masterpieces'){
+        youprefer = '<img src="image/piece.jpg"/>';
+    }else if ( product === 'accessories'){
+        youprefer = '<img src="./image/mpiece.jpg"/>';
+    }
+  // }
+   function pieces(){
+    var noof = prompt ('how many pieces do you want?');
+    
+    var result ='';
   
-  }
-  
-  document.write(image)
-  
-  }
-  
-  greeting();
-  orderingCookie();
+    for (var i = 0 ; i < noof ; i++){
+    result = result + youprefer;
+    
+    }
+    
+    document.write(result);
+    alert(' :) ')
+}
+product();
+//Product1();
+pieces();
+
+
