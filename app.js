bd40706a2bf2174be317c11ba6d0e12e5377b05e
mais dmoor <maisdmoor1995@gmail.com>
2021-01-31 18:24:39 +0200

    m

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 
-function getName() {
-    var userName = prompt('Enter your name?');
-    console.log(typeof prompt);
-    console.log(userName);
-    for (var i = 0; i <= 10; i++) {
-      console.log(i);
+var product = prompt ('masterpieces or accessories');
+
+while (product !== 'masterpieces' && product !== 'accessories'){
+
+    product = prompt ('masterpieces or accessories');
+    
     }
-  document.write(userName);
-};
+    
+    var youprefer = '';
+    
+    if ( product === 'masterpieces'){
+        youprefer = '<img src="./image/piece.jpg "/>';
+    }else if ( product === 'accessories'){
+        youprefer = '<img src="./image/mpiece.jpg"/>';
+    }
+    
+    var noof = prompt ('how many pieces do you want?');
+    var result ='';
+    
+    for (var i = 0 ; i < noof ; i++){
+    result = result + youprefer;
+    
+    }
+    
+    document.write(result);
+    alert(' :) ');
